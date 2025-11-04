9dc725bd61d5f6b5b8e3a8aad560c0a04bf2a27a
MinZee146 <minhduy146204@gmail.com>
2025-11-04 12:49:24 +0700

    Add mock data for Notification

--- src/mock/db.ts
+++ src/mock/db.ts
@@ -43,3 +43,44 @@ export const stories = users.map((u, i) => ({
   thumbUrl: u.avatarUrl,
   hasNew: i % 2 === 0,
 }));
+
+export const notificationsData = [
+  {
+    id: 1,
+    section: "Today",
+    avatar: "https://i.pravatar.cc/150?img=5",
+    message: "Ms.chinh, B Ray and 2 others recently shared 5 posts.",
+    time: "19h",
+    unread: true,
+    icon: "book", // optional icon name
+  },
+  {
+    id: 2,
+    section: "Earlier",
+    avatar: "https://i.pravatar.cc/150?img=15",
+    message:
+      "Thuan Vo commented on your post in Chợ GearVN 2nd: Logitech-Zowie-Hyperx-Razer...",
+    time: "3d",
+  },
+  {
+    id: 3,
+    section: "Earlier",
+    avatar: "https://i.pravatar.cc/150?img=10",
+    message:
+      "adidas posted a new reel: 'BÙNG NỔ SỰ KIỆN KHAI TRƯƠNG CỬA HÀNG HOME OF SPORTS VI...'",
+    time: "4d",
+    icon: "youtube",
+  },
+  {
+    id: 4,
+    section: "Earlier",
+    avatar: "https://i.pravatar.cc/150?img=3",
+    message:
+      "The Fire Monkey, a Page you recently viewed, invited you to join their private group Săn deal cùng...",
+    time: "5d",
+    actions: [
+      { label: "Join", type: "primary" },
+      { label: "Delete", type: "secondary" },
+    ],
+  },
+]
