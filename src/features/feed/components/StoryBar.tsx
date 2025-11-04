3666b25832c3118d0f553254de7422ba139cf5a3
NHHIEN <hien20040908@gmail.com>
2025-11-04 15:06:28 +0700

    Update storybar

--- src/features/feed/components/StoryBar.tsx
+++ src/features/feed/components/StoryBar.tsx
@@ -1,17 +1,47 @@
-import React, { memo } from 'react'
+import { memo } from 'react'
 import { ScrollView } from 'react-native'
-import { XStack } from 'tamagui'
+import { XStack, YStack, Text, Button } from 'tamagui'
+import { Plus } from '@tamagui/lucide-icons'
 import { stories } from '@/mock/db'
+import Avatar from '@/components/Avatar'
 import StoryItem from './StoryItem'
+
+function CreateStoryItem() {
+  const userAvatar = 'https://i.pravatar.cc/100?img=40'
+
+  return (
+    <YStack alignItems="center" marginHorizontal="$1" width={90}>
+      <YStack  padding={2}  borderRadius={999}  borderWidth={2} borderColor="$red10"  >
+        <Avatar uri={userAvatar} size={85} />
+        <Button
+          size="$2"
+          circular
+          icon={Plus}
+          backgroundColor="$blue10"
+          color="white"
+          position="absolute"
+          right={0}
+          bottom={0}
+          borderWidth={3}
+          borderColor="white"
+        />
+      </YStack>
+      <Text numberOfLines={1} marginTop="$1" fontSize={14} opacity={0.8}>
+        Your Story
+      </Text>
+    </YStack>
+  )
+}
 
 function StoryBar() {
   return (
     <ScrollView
       horizontal
       showsHorizontalScrollIndicator={false}
-      contentContainerStyle={{ paddingHorizontal: 12 }}
+      contentContainerStyle={{ paddingVertical: 12, paddingHorizontal: 12 }}
     >
       <XStack>
+        <CreateStoryItem />
         {stories.map((s) => (
           <StoryItem key={s.id} story={{ ...s, thumbUrl: s.thumbUrl ?? '' }} />
         ))}
--- src/features/feed/components/StoryItem.tsx
+++ src/features/feed/components/StoryItem.tsx
@@ -1,35 +1,24 @@
-import Avatar from "@/components/Avatar"
-import { Story } from "@/types/models"
-import { YStack, Text } from "tamagui"
+import Avatar from '@/components/Avatar'
+import { Story } from '@/types/models'
+import React from 'react'
+import { YStack, Text } from 'tamagui'
 
 function StoryItem({ story }: { story: Story }) {
   const { author, hasNew } = story
+  const borderColor = hasNew ? '$red10' : '$blue10' 
 
   return (
-    <YStack alignItems="center" marginHorizontal="$2" minWidth={84}>
-      <YStack position="relative">
-        <Avatar uri={author.avatarUrl ?? ''} size={64} />
-        {hasNew && (
-          <YStack
-            position="absolute"
-            right={-2}
-            bottom={-2}
-            width={20}
-            height={20}
-            borderRadius={999}
-            backgroundColor="#1877F2"
-            alignItems="center"
-            justifyContent="center"
-            borderColor="#fff"
-            borderWidth={2}
-          >
-            <Text color="white" fontWeight="800">
-              +
-            </Text>
-          </YStack>
-        )}
+    <YStack alignItems="center" marginHorizontal="$1" width={100}>
+      <YStack
+        padding={2}
+        borderRadius={999}
+        borderWidth={2}
+        borderColor={borderColor} 
+      >
+        <Avatar uri={author.avatarUrl ?? ''} size={85} />
       </YStack>
-      <Text numberOfLines={1} marginTop="$1" fontSize={12} opacity={0.8}>
+
+      <Text numberOfLines={1} marginTop="$1" fontSize={13} opacity={0.8}>
         {author.username}
       </Text>
     </YStack>
