68523a913ebbf21f37c2450d92c85519a329e9d8
NHHIEN <hien20040908@gmail.com>
2025-11-04 15:08:41 +0700

    Minor

--- src/mock/db.ts
+++ src/mock/db.ts
@@ -5,6 +5,14 @@ const users: User[] = [
     avatarUrl: "https://i.pravatar.cc/150?img=1" },
   { id: "u2", username: "pentoro.studio", name: "Pentoro Studio",
     avatarUrl: "https://i.pravatar.cc/150?img=12" },
+  { id: "u1", username: "hien.dev", name: "Nguyen Hoang Hien",
+    avatarUrl: "https://i.pravatar.cc/150?img=1" },
+  { id: "u2", username: "pentoro.studio", name: "Pentoro Studio",
+    avatarUrl: "https://i.pravatar.cc/150?img=12" },
+  { id: "u1", username: "hien.dev", name: "Nguyen Hoang Hien",
+    avatarUrl: "https://i.pravatar.cc/150?img=1" },
+  { id: "u2", username: "pentoro.studio", name: "Pentoro Studio",
+    avatarUrl: "https://i.pravatar.cc/150?img=12" },
 ];
 
 export const posts: Post[] = [
