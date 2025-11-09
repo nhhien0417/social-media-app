246313f5e59079cb7fa145a43bf15ad1d7fc36ad
MinZee146 <minhduy146204@gmail.com>
2025-11-09 16:01:03 +0700

    Push luon

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -13,7 +13,7 @@ const getBaseURL = () => {
   if (Platform.OS === 'web') {
     return 'http://localhost:1208/api/v1'
   }
-  return 'http://192.168.1.9:1208/api/v1'
+  return 'http://192.168.1.39:1208/api/v1'
 }
 
 export const API_BASE_URL = getBaseURL()
