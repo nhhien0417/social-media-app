11c748100abf86a51796317680f47be585338eae
NHHIEN <hien20040908@gmail.com>
2025-12-04 23:09:54 +0700

    Update local base url

--- src/utils/BaseUrl.ts
+++ src/utils/BaseUrl.ts
@@ -4,7 +4,7 @@ const getBaseURL = () => {
   if (Platform.OS === 'web') {
     return 'http://localhost:1208/api/v1'
   }
-  return 'http://192.168.1.6:1208/api/v1'
+  return 'http://192.168.1.8:1208/api/v1'
 }
 
 export const API_BASE_URL = getBaseURL()
